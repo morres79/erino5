e1540ea1254d23bc12a65af1843bc164bbe9ab2f
morres79 <morres79@gmail.com>
2026-01-01 15:23:32 +0300

    about_house.php

---
+++ about_house.php
@@ -0,0 +1,385 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!--?php echo $titlePage?-->
+  <title>ТСЖ &quot;Еринские Высоты&quot/Информация о доме</title>
+</head>
+
+<body>
+
+  <?php include_once 'header.php'; ?>
+ 
+<section>
+    <div class="conteiner">
+      <?php include 'vmenu.php'; ?>
+
+        <div class="inf_frame">
+            <div class="inf_main_header">Информация о доме</div>
+            <div class = "inf_wraper">
+                
+                <div class = "inf_header"> Расположение</div>
+                
+                
+                <td class =  "MaterialImage"><img alt = "Logo" src="images/dom5_2.jpg" ><img alt = "Logo" src="images/dom5_1.jpg" ></td>
+                <td align="left" valign="middle" class =  "MaterialShortInfo"><b>Адрес:</b> 108822, Моска,  сельское поселение Рязановское,
+                    поселок Ерино, ул. Высокая, д. 5. <br>
+                    <br>
+                    Поселок Ерино расположен на границе г. Подольска, в 17 км от МКАД в живописном месте на берегу реки Пахра. 
+                    Рядом располагается всеми известный своими лечебными минеральными водами пансионат &laquo;Ерино&raquo;. <br>
+                    <br>
+                    <br>
+                </td>
+            </div>
+            <div class = "inf_wraper">
+                
+                <div class = "inf_header"> Планировки квартир </div>
+                
+                
+                <td class =  "MaterialShortInfo" style ="TEXT-ALIGN: left"><img alt = "Планировки" src="images/dom5_plan_2.jpg" > <br>
+                    Высота потолков 3 метра </td>
+                
+            </div>
+            <div class = "inf_wraper">
+                            
+                                <div class = "inf_header"> Поэтажный список квартир </div>
+                            
+                            <!--Текст-->
+                            
+                                <td class =  "MaterialShortInfo" style ="TEXT-ALIGN: center"><table class = "MaterialPanel" border="1" cellpadding="3" cellspacing="0">
+                                
+                                    <td>                      
+                                    <td colspan = "5"><b>1 секция</b><br>
+                                    (ближе к дороге на Сальково) </td>
+                                    <td colspan = "5"><b>2 секция</b><br>
+                                    (ближе к военной части) </td>
+                                    <td>                      
+                                
+                                
+                                    <td class="FlatList_Header">Номер на этаже</td>
+                                    <td class="FlatList_Header">№1<br>
+                                    3комн 96м</td>
+                                    <td class="FlatList_Header">№2<br>
+                                    2комн 63м</td>
+                                    <td class="FlatList_Header">№3<br>
+                                    1комн 43м</td>
+                                    <td class="FlatList_Header">№4<br>
+                                    1комн 43м</td>
+                                    <td class="FlatList_Header">№5<br>
+                                    2комн 76м</td>
+                                    <td class="FlatList_Header">№1<br>
+                                    2комн 76м</td>
+                                    <td class="FlatList_Header">№2<br>
+                                    1комн 43м</td>
+                                    <td class="FlatList_Header">№3<br>
+                                    1комн 43м</td>
+                                    <td class="FlatList_Header">№4<br>
+                                    2комн 63м</td>
+                                    <td class="FlatList_Header">№5<br>
+                                    3комн 96м</td>
+                                    <td class="FlatList_Header">                      
+                                
+                                
+                                    <td>17 этаж</td>
+                                    <td>81</td>
+                                    <td>82</td>
+                                    <td>83</td>
+                                    <td>84</td>
+                                    <td>85</td>
+                                    <td>166</td>
+                                    <td>167</td>
+                                    <td>168</td>
+                                    <td>169</td>
+                                    <td>170</td>
+                                    <td>17 этаж</td>
+                                
+                                
+                                    <td>16 этаж</td>
+                                    <td>76</td>
+                                    <td>77</td>
+                                    <td>78</td>
+                                    <td>79</td>
+                                    <td>80</td>
+                                    <td>161</td>
+                                    <td>162</td>
+                                    <td>163</td>
+                                    <td>164</td>
+                                    <td>165</td>
+                                    <td>16 этаж</td>
+                                
+                                
+                                    <td>15 этаж</td>
+                                    <td>71</td>
+                                    <td>72</td>
+                                    <td>73</td>
+                                    <td>74</td>
+                                    <td>75</td>
+                                    <td>156</td>
+                                    <td>157</td>
+                                    <td>158</td>
+                                    <td>159</td>
+                                    <td>160</td>
+                                    <td>15 этаж</td>
+                                
+                                
+                                    <td>14 этаж</td>
+                                    <td>66</td>
+                                    <td>67</td>
+                                    <td>68</td>
+                                    <td>69</td>
+                                    <td>70</td>
+                                    <td>151</td>
+                                    <td>152</td>
+                                    <td>153</td>
+                                    <td>154</td>
+                                    <td>155</td>
+                                    <td>14 этаж</td>
+                                
+                                
+                                    <td>13 этаж</td>
+                                    <td>61</td>
+                                    <td>62</td>
+                                    <td>63</td>
+                                    <td>64</td>
+                                    <td>65</td>
+                                    <td>146</td>
+                                    <td>147</td>
+                                    <td>148</td>
+                                    <td>149</td>
+                                    <td>150</td>
+                                    <td>13 этаж</td>
+                                
+                                
+                                    <td>12 этаж</td>
+                                    <td>56</td>
+                                    <td>57</td>
+                                    <td>58</td>
+                                    <td>59</td>
+                                    <td>60</td>
+                                    <td>141</td>
+                                    <td>142</td>
+                                    <td>143</td>
+                                    <td>144</td>
+                                    <td>145</td>
+                                    <td>12 этаж</td>
+                                
+                                
+                                    <td>11 этаж</td>
+                                    <td>51</td>
+                                    <td>52</td>
+                                    <td>53</td>
+                                    <td>54</td>
+                                    <td>55</td>
+                                    <td>136</td>
+                                    <td>137</td>
+                                    <td>138</td>
+                                    <td>139</td>
+                                    <td>140</td>
+                                    <td>11 этаж</td>
+                                
+                                
+                                    <td>10 этаж</td>
+                                    <td>46</td>
+                                    <td>47</td>
+                                    <td>48</td>
+                                    <td>49</td>
+                                    <td>50</td>
+                                    <td>131</td>
+                                    <td>132</td>
+                                    <td>133</td>
+                                    <td>134</td>
+                                    <td>135</td>
+                                    <td>10 этаж</td>
+                                
+                                
+                                    <td>9 этаж</td>
+                                    <td>41</td>
+                                    <td>42</td>
+                                    <td>43</td>
+                                    <td>44</td>
+                                    <td>45</td>
+                                    <td>126</td>
+                                    <td>127</td>
+                                    <td>128</td>
+                                    <td>129</td>
+                                    <td>150</td>
+                                    <td>9 этаж</td>
+                                
+                                
+                                    <td>8 этаж</td>
+                                    <td>36</td>
+                                    <td>37</td>
+                                    <td>38</td>
+                                    <td>39</td>
+                                    <td>40</td>
+                                    <td>121</td>
+                                    <td>122</td>
+                                    <td>123</td>
+                                    <td>124</td>
+                                    <td>125</td>
+                                    <td>8 этаж</td>
+                                
+                                
+                                    <td>7 этаж</td>
+                                    <td>31</td>
+                                    <td>32</td>
+                                    <td>33</td>
+                                    <td>34</td>
+                                    <td>35</td>
+                                    <td>116</td>
+                                    <td>117</td>
+                                    <td>118</td>
+                                    <td>119</td>
+                                    <td>120</td>
+                                    <td>7 этаж</td>
+                                
+                                
+                                    <td>6 этаж</td>
+                                    <td>26</td>
+                                    <td>27</td>
+                                    <td>28</td>
+                                    <td>29</td>
+                                    <td>30</td>
+                                    <td>111</td>
+                                    <td>112</td>
+                                    <td>113</td>
+                                    <td>114</td>
+                                    <td>115</td>
+                                    <td>6 этаж</td>
+                                
+                                
+                                    <td>5 этаж</td>
+                                    <td>21</td>
+                                    <td>22</td>
+                                    <td>23</td>
+                                    <td>24</td>
+                                    <td>25</td>
+                                    <td>106</td>
+                                    <td>107</td>
+                                    <td>108</td>
+                                    <td>109</td>
+                                    <td>110</td>
+                                    <td>5 этаж</td>
+                                
+                                
+                                    <td>4 этаж</td>
+                                    <td>16</td>
+                                    <td>17</td>
+                                    <td>18</td>
+                                    <td>19</td>
+                                    <td>20</td>
+                                    <td>101</td>
+                                    <td>102</td>
+                                    <td>103</td>
+                                    <td>104</td>
+                                    <td>105</td>
+                                    <td>4 этаж</td>
+                                
+                                
+                                    <td>3 этаж</td>
+                                    <td>11</td>
+                                    <td>12</td>
+                                    <td>13</td>
+                                    <td>14</td>
+                                    <td>15</td>
+                                    <td>96</td>
+                                    <td>97</td>
+                                    <td>98</td>
+                                    <td>99</td>
+                                    <td>100</td>
+                                    <td>3 этаж</td>
+                                
+                                
+                                    <td>2 этаж</td>
+                                    <td>6</td>
+                                    <td>7</td>
+                                    <td>8</td>
+                                    <td>9</td>
+                                    <td>10</td>
+                                    <td>91</td>
+                                    <td>92</td>
+                                    <td>93</td>
+                                    <td>94</td>
+                                    <td>95</td>
+                                    <td>2 этаж</td>
+                                
+                                
+                                    <td>1 этаж</td>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    <td>4</td>
+                                    <td>5</td>
+                                    <td>86</td>
+                                    <td>87</td>
+                                    <td>88</td>
+                                    <td>89</td>
+                                    <td>90</td>
+                                    <td>1 этаж</td>
+                                
+                                    </td>
+                            
+            </div>
+            <div class = "inf_wraper">
+                            
+                                <div class = "inf_header"> Инфраструктура </div>
+                            
+                            
+                                <td align="left" valign="top" class =  "MaterialImage"><img alt = "Logo" src="images/dom5_4.jpg" ></td>
+                                <td align="left" valign="top" class =  "MaterialShortInfo"><b>Окружающая инфраструктура</b> включает: <br>
+                                <br>
+                                - охраняемую автостоянку <br>
+                                - новый детский садик <br>
+                                - новую современную общеобразовательную школу на 800 мест (строительство закончено в 2009 году) <br>
+                                - спортивный комплекс &laquo;Богатырь&raquo; с тренажерным залом, спортивными площадками и 
+                                кортом <br>
+                                - детскую школу искусств &laquo;Синяя птица&raquo; где Ваши дети смогут получить 
+                                дополнительное образование в различных кружках и на курсах (танцы, курсы 
+                                актерского мастерства и многие другие) <br>
+                                - новый стадион европейского класса (строительство закончено в 2009 году) <br>
+                                - бассейн в Санатории "Ерино" <br>
+                                <br>
+                                <b>Все объекты находятся в шаговой доступности от дома.</b></td>
+                            
+            </div>
+            <div class = "inf_wraper">
+                
+                <div class = "inf_header"> Общественный транспорт </div>
+                
+                
+                <td align="left" valign="top" class =  "MaterialShortInfo"><b>Из Москвы</b> (время в пути без пробок - 30-40 мин): <br>
+                    <br>
+                    - Автобус №462 от м. Южная до пос. Ерино (по расписанию) <br>
+                    - любые автобусы, маршрутные такси из Москвы в Подольск, следующие через остановку "Красная Горка" <br>
+                    (до остановки "Красная Горка", затем на автобусе или маршрутном такси 1022 или 1041 до остановки &quot;Сады&quot;).<br>
+                    <br>
+                    <b>Из Подольска</b>: <br>
+                    <br>
+                    - 10 мин на автобусе или маршрутном такси 1022 от ж. д. станции "Подольск" (интервал движения 10-15 мин).</td>
+                
+            </div>
+            <div class = "inf_wraper">
+                
+                <div class = "inf_header"> Схема проезда из Москвы </div>
+                
+                
+                <td align="left" valign="top" class =  "MaterialShortInfo" style ="TEXT-ALIGN: left"><img alt = "proezd plan" src="images/dom5_proezd.jpg" > <br>
+                    <br>
+                    Вид со спутника: <br>
+                    <img alt = "proezd sputnik" src="images/dom5_sputnik.jpg" > <br></td>
+                
+            </div>
+
+        </div> <!-- inf_frame -->
+    </div> <!-- conteiner -->
+</section>
+
+  <!--Подвал-->
+  <?php include_once 'footer.php'; ?>
+
+</body>
+
+</html>
